420fed5ad458cd141fe359de9baced0cd9575e3c
Arsalan Ahmad <you@example.com>
2022-10-09 13:21:48 -0700

    react mini

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        marginTop: "25%",
+      }}
+    >
+      <h1 style={{ fontWeight: "bold" }}>React Mini</h1>
     </div>
   );
 }
